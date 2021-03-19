126d6af19efa0848f3e59a97e82ee9cc11705c48
Komal Arora <kay.arora@outlook.com>
2021-03-19 00:54:00 -0500

    linked list

---
+++ CodingProblems/CodingProblems/EasyLevel/LinkedLists/RemoveLinkedListElements.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingProblems.EasyLevel.LinkedLists
+{
+    public static class RemoveLinkedListElements
+    {
+        public static ListNode RemoveElementsFromList(ListNode head, int val)
+        {
+            
+            var dummy = new ListNode(0);
+            var cur = dummy;
+            while (head != null)
+            {
+                if (head.val != val)
+                {
+                    cur.next = head;
+                    cur = cur.next;
+                }
+                head = head.next;
+            }
+            cur.next = null;
+            return dummy.next;
+        }
+    }
+}
